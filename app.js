9ea6a2007cc8d41496002deda3ff45635d1f1a06
superEarth999 <earthmernstack@gmail.com>
2024-03-26 16:03:49 +0530

    update

--- app.js
+++ app.js
@@ -6,3 +6,21 @@ document.addEventListener('DOMContentLoaded', function () {
         menuItems.classList.toggle('hidden');
     });
 });
+
+
+    document.addEventListener("DOMContentLoaded", function () {
+        const menuButton = document.getElementById("menuButton");
+        const menuIcon = document.getElementById("menuIcon");
+        const crossIcon = document.getElementById("crossIcon");
+
+        menuButton.addEventListener("click", function () {
+            if (menuIcon.classList.contains("hidden")) {
+                menuIcon.classList.remove("hidden");
+                crossIcon.classList.add("hidden");
+            } else {
+                menuIcon.classList.add("hidden");
+                crossIcon.classList.remove("hidden");
+            }
+        });
+    });
+
